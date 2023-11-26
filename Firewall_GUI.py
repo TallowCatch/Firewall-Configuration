2ce2bc86ff99ac732763211aea18d41bc3e54928
Ameer <73332976+TallowCatch@users.noreply.github.com>
2023-11-27 01:27:11 +0400

    Update Firewall_GUI.py

--- Firewall_GUI.py
+++ Firewall_GUI.py
@@ -7,9 +7,15 @@ import socket
 import datetime
 import requests
 import webbrowser
+import os
 
 customtkinter.set_appearance_mode("dark") 
 customtkinter.set_default_color_theme("dark-blue") 
+
+# Function to load images using relative paths
+def load_image(image_name):
+    image_path = os.path.join("Images", image_name)
+    return ImageTk.PhotoImage(Image.open(image_path).resize((20, 20), Image.Resampling.LANCZOS))
 
 class FirewallGUI:
     def __init__(self, master):
@@ -19,16 +25,16 @@ class FirewallGUI:
         # Styling
         style = ttk.Style()
         style.configure('TButton', background='#4a7a8c', font=('Arial', 10))
-        style.configure('TLabel', font=('Arial', 12), background='#334257', foreground='#ffffff')  # Enlarged font size for labels
+        style.configure('TLabel', font=('Arial', 12), background='#334257', foreground='#ffffff')
         master.configure(bg='#334257')
 
         # Server connection details
         self.server_host = '127.0.0.1'
         self.server_port = 2000
 
-        #Labels
-        ttk.Label(master, text="Insert the IP Address here \u279C", style='TLabel').grid(row=0, column=0, padx=10, pady=10)  # Unicode arrow
-        ttk.Label(master, text="Insert the Port here \u279C", style='TLabel').grid(row=1, column=0, padx=10, pady=10)  # Unicode arrow
+        # Labels
+        ttk.Label(master, text="Insert the IP Address here \u279C", style='TLabel').grid(row=0, column=0, padx=10, pady=10)
+        ttk.Label(master, text="Insert the Port here \u279C", style='TLabel').grid(row=1, column=0, padx=10, pady=10)
 
         # Entry fields
         self.ip_entry = ttk.Entry(master)
@@ -37,15 +43,14 @@ class FirewallGUI:
         self.port_entry.grid(row=1, column=1, padx=10, pady=10)
 
         # Load icons for buttons
-        # Note: Make sure to update these paths to your icons
-        self.add_icon = ImageTk.PhotoImage(Image.open("/Users/ameerfiras/Desktop/C_Project/Images/plus-icon-vector.png").resize((20, 20), Image.Resampling.LANCZOS))
-        self.delete_icon = ImageTk.PhotoImage(Image.open("/Users/ameerfiras/Desktop/C_Project/Images/delete-icon-vector.png").resize((20, 20), Image.Resampling.LANCZOS))
-        self.list_icon = ImageTk.PhotoImage(Image.open("/Users/ameerfiras/Desktop/C_Project/Images/list-icon-vector.png").resize((20, 20), Image.Resampling.LANCZOS))
-        self.check_icon = ImageTk.PhotoImage(Image.open("/Users/ameerfiras/Desktop/C_Project/Images/IP_Image.png").resize((20, 20), Image.Resampling.LANCZOS))
+        self.add_icon = load_image("plus-icon-vector.png")
+        self.delete_icon = load_image("delete-icon-vector.png")
+        self.list_icon = load_image("list-icon-vector.png")
+        self.check_icon = load_image("IP_Image.png")
 
-        # Set fixed button sizes
-        button_width = 190  # or any suitable value
-        button_height = 40  # or any suitable value
+        # Button configurations
+        button_width = 190
+        button_height = 40
 
         # Buttons
         customtkinter.CTkButton(master, image=self.add_icon, text="Add", width=button_width, height=button_height, compound="top", command=self.add_rule).grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
@@ -53,7 +58,7 @@ class FirewallGUI:
         customtkinter.CTkButton(master, image=self.list_icon, text="List", width=button_width, height=button_height, compound="top", command=self.list_rules).grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
         customtkinter.CTkButton(master, image=self.check_icon, text="Check", width=button_width, height=button_height, compound="top", command=self.check_rule).grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
 
-        # Configure the grid to control stretching
+        # Grid configuration
         master.grid_rowconfigure(3, weight=0)
         master.grid_rowconfigure(4, weight=0)
         master.grid_columnconfigure(0, weight=1)
@@ -67,30 +72,20 @@ class FirewallGUI:
         menubar.add_cascade(label="Help", menu=helpmenu)
         master.config(menu=menubar)
 
-        # Configure the grid to control stretching
-        for i in range(5):  # Configure rows 0 to 4 to not expand
-            master.grid_rowconfigure(i, weight=0)
-        master.grid_columnconfigure(0, weight=1)
-        master.grid_columnconfigure(1, weight=1)
-
         # Log window
         self.log_text = scrolledtext.ScrolledText(master, height=10, state=tk.DISABLED)
         self.log_text.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
-        master.grid_rowconfigure(5, weight=1)  # Make the log window row expand
-
-        # Make the log window expandable
-        self.log_text.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        master.grid_rowconfigure(5, weight=1)
 
     # Help section methods
     def show_about(self):
         messagebox.showinfo("About", "Firewall Rule Manager\nVersion 1.0")
 
     def show_usage(self):
-        # Use webbrowser to open the README.md file in the default browser
         readme_path = 'https://github.com/TallowCatch/Firewall-Configuration/blob/main/README.md'
         webbrowser.open_new(readme_path)
 
-     # Validation functions
+    # Validation functions
     def validate_ip(self):
         ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
         ip = self.ip_entry.get()
@@ -108,39 +103,28 @@ class FirewallGUI:
             return False
         return True
 
-    # Add the log_message method
-        
+    # Log message method
     def log_message(self, message):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        # Check if the message contains rule-query pairs
         if "Rule:" in message:
-            # Split the message into lines and process each line
             lines = message.split('\n')
             formatted_lines = []
             first_rule = True
             for line in lines:
-                # Insert a newline before each "Rule:" line except the first
                 if line.startswith("Rule:") and not first_rule:
                     formatted_lines.append("\n" + line)
                 else:
                     formatted_lines.append(line)
                 if line.startswith("Rule:"):
                     first_rule = False
-            # Join the processed lines back into a single string
             message = '\n'.join(formatted_lines)
-
-        # Prepare the message with dashed lines
         formatted_message = f"-----\n\n{timestamp} - {message}\n\n-----\n"
-
-        # Temporarily enable the widget to add text
         self.log_text.config(state=tk.NORMAL)
         self.log_text.insert(tk.END, formatted_message)
-
-        # Disable the widget to prevent user input
         self.log_text.config(state=tk.DISABLED)
-        self.log_text.yview(tk.END)  # Auto-scroll to the bottom
+        self.log_text.yview(tk.END)
 
+    # Server communication methods
     def communicate_with_server(self, message):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
